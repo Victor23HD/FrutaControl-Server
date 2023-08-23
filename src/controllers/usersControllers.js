4a5f8a23fd10f7f3c69375a35b2c9bfea3755c7e
Victor Oliveira <xitornet.23@gmail.com>
2023-08-22 22:06:17 -0300

    feat: add login authentication
    Adds login authentication functionality to the "checkLogin" endpoint. Now, on receiving a login request with a valid email and password, the system generates a valid JWT token for the authenticated user.
    
    - Added code to check that the password provided matches the password stored in the database.
    
    - Generated and signed a JWT token containing the authenticated user's ID.
    
    - Returned the generated token as part of the success response (status 201).
    
    - Added error response (status 404) for cases where the user is not found in the database.

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,8 +1,92 @@
+<<<<<<< Updated upstream
 const usersControllers =
 {
     listUsers: async(req, res) => {
         res.status(200).send("Victor | 12345");
+=======
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const usersControllers = {
+  listUsers: async (req, res) => {
+    res.status(200).send(user);
+  },
+
+  registerUsers: async (req, res) => {
+    const { username, password, email, confirmPassword } = req.body;
+
+    // Check e-mail
+    if (await User.findOne({ email: email })) {
+      return res.status(422).json({ msg: "Please use another e-mail!" });
+>>>>>>> Stashed changes
     }
 }
 
+<<<<<<< Updated upstream
 export default usersControllers;
+=======
+    // Create password
+
+    const abcde = bcrypt.genSalt(12, (error, salt) => {
+      if (error) {
+        console.error("Error generating salt:", error);
+        return;
+      }
+
+      if (password !== confirmPassword) {
+        return res.status(422).json({ msg: "The passwords don't match" });
+      }
+
+      // Use the generated salt to hash the password
+      bcrypt.hash(password, salt, async (hashError, hash) => {
+        if (hashError) {
+          console.error("Error generating hash:", hashError);
+          return;
+        }
+
+        // Create User
+        const user = new User({
+          name: username,
+          email: email,
+          password: hash,
+        });
+
+        try {
+          await user.save();
+          res.status(201).json({ msg: "User created successfully!" });
+        } catch (error) {
+          res.status(422).json({ msg: "There was an error internally!" });
+        }
+      });
+    });
+  },
+
+  checkLogin: async (req, res) => {
+    const { email, password } = req.body;
+
+    const _user = await User.findOne({ email: email });
+   
+
+    const checkPass = bcrypt.compare(password, _user.password);
+
+    if (checkPass) {
+      const secret = process.env.SECRET;
+
+      const token = jwt.sign(
+        {
+          id: _user._id,
+        },
+        secret
+      );
+
+      res.status(201).json({msg: `TOKEN: ${token}`});
+
+    }else{
+        res.status(404).json({msg: "User not found!"});
+    }
+  },
+};
+
+export default usersControllers;
+>>>>>>> Stashed changes
